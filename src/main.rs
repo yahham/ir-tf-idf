036608cb2c2008fa27a4d20333e0ab2b26c10272
Yahia Hamitou <hamitou.yah@gmail.com>
2026-02-18 19:36:03 +0100

    Add xml-rs dependency and implement basic file reading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::fs;
+use std::process::exit;
 fn main() {
-    println!("Hello, world!");
+    let file_path = "dataset/glClear.xhtml";
+    let content = fs::read_to_string(file_path).unwrap_or_else(|err| {
+        eprintln!("ERROR: could not read file {file_path}: {err}");
+        exit(1)
+    });
+    println!("Length of {file_path} is {length}", length = content.len());
 }
