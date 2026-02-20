1c42abe6570c5d77bca8da690ddf69a3c8f9c9e7
Yahia Hamitou <hamitou.yah@gmail.com>
2026-02-20 18:40:03 +0100

    Implement XML text extraction using EventReader

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,31 @@
-use std::fs;
-use std::process::exit;
-fn main() {
-    let file_path = "dataset/glClear.xhtml";
-    let content = fs::read_to_string(file_path).unwrap_or_else(|err| {
-        eprintln!("ERROR: could not read file {file_path}: {err}");
-        exit(1)
-    });
-    println!("Length of {file_path} is {length}", length = content.len());
+use std::fs::{self, File};
+use std::io;
+use std::path::Path;
+use xml::reader::{EventReader, XmlEvent};
+
+fn read_entire_xml_file<P: AsRef<Path>>(file_path: P) -> io::Result<String> {
+    let file = File::open(file_path)?;
+    let er = EventReader::new(file);
+    let mut content = String::new();
+    for event in er.into_iter() {
+        if let XmlEvent::Characters(text) = event.expect("TODO") {
+            content.push_str(&text);
+        }
+    }
+    Ok(content)
+}
+
+fn main() -> io::Result<()> {
+    let dir_path = "dataset/docs.gl/gl4";
+    let dir = fs::read_dir(dir_path)?;
+    for file in dir {
+        let file_path = file?.path();
+        let content = read_entire_xml_file(&file_path)?;
+        println!("{file_path:?} => {size}", size = content.len());
+    }
+    // println!(
+    //     "{content}",
+    //     content = read_entire_xml_file(file_path).expect("TODO")
+    // );
+    Ok(())
 }
