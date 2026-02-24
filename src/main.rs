8da7b2441fb535b6c627f2bda1fdbd70f61f00d9
Yahia Hamitou <hamitou.yah@gmail.com>
2026-02-24 08:32:50 +0100

    Implement Lexer for tokenizing XML text content

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,70 @@
-use std::fs::{self, File};
+// use std::collections::HashMap;
+use std::fs::File;
 use std::io;
 use std::path::Path;
 use xml::reader::{EventReader, XmlEvent};
+
+#[derive(Debug)]
+struct Lexer<'a> {
+    content: &'a [char],
+}
+
+impl<'a> Lexer<'a> {
+    fn new(content: &'a [char]) -> Self {
+        Self { content }
+    }
+
+    fn trim_left(&mut self) {
+        while self.content.len() > 0 && self.content[0].is_whitespace() {
+            self.content = &self.content[1..];
+        }
+    }
+
+    fn chop(&mut self, n: usize) -> &'a [char] {
+        let token = &self.content[0..n];
+        self.content = &self.content[n..];
+        token
+    }
+
+    fn chop_while<P>(&mut self, mut predicate: P) -> &'a [char]
+    where
+        P: FnMut(&char) -> bool,
+    {
+        let mut n = 0;
+        while n < self.content.len() && predicate(&self.content[n]) {
+            n += 1;
+        }
+        self.chop(n)
+    }
+
+    fn next_token(&mut self) -> Option<&'a [char]> {
+        self.trim_left();
+        if self.content.len() == 0 {
+            return None;
+        }
+
+        if self.content[0].is_numeric() {
+            return Some(self.chop_while(|x| x.is_numeric()));
+        }
+
+        if self.content[0].is_alphabetic() {
+            return Some(self.chop_while(|x| x.is_alphanumeric()));
+        }
+        return Some(self.chop(1));
+    }
+}
+
+impl<'a> Iterator for Lexer<'a> {
+    type Item = &'a [char];
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next_token()
+    }
+}
+
+// fn index_document(_doc_content: &str) -> HashMap<String, usize> {
+//     todo!("not implmented");
+// }
 
 fn read_entire_xml_file<P: AsRef<Path>>(file_path: P) -> io::Result<String> {
     let file = File::open(file_path)?;
@@ -10,22 +73,34 @@ fn read_entire_xml_file<P: AsRef<Path>>(file_path: P) -> io::Result<String> {
     for event in er.into_iter() {
         if let XmlEvent::Characters(text) = event.expect("TODO") {
             content.push_str(&text);
+            content.push_str(" ");
         }
     }
     Ok(content)
 }
 
 fn main() -> io::Result<()> {
-    let dir_path = "dataset/docs.gl/gl4";
-    let dir = fs::read_dir(dir_path)?;
-    for file in dir {
-        let file_path = file?.path();
-        let content = read_entire_xml_file(&file_path)?;
-        println!("{file_path:?} => {size}", size = content.len());
+    let content = read_entire_xml_file("dataset/reviews/www.austinchronicle.com/21.xml")?
+        .chars()
+        .collect::<Vec<_>>();
+
+    for token in Lexer::new(&content) {
+        println!(
+            "{token}",
+            token = token
+                .iter()
+                .map(|x| x.to_ascii_uppercase())
+                .collect::<String>()
+        );
     }
-    // println!(
-    //     "{content}",
-    //     content = read_entire_xml_file(file_path).expect("TODO")
-    // );
+    // let all_documents: HashMap<Path, HashMap<String, usize>>::new();
+
+    // let dir_path = "dataset/reviews/www.austinchronicle.com";
+    // let dir = fs::read_dir(dir_path)?;
+    // for file in dir {
+    //     let file_path = file?.path();
+    //     let content = read_entire_xml_file(&file_path)?;
+    //     println!("{file_path:?} => {size}", size = content.len());
+    // }
     Ok(())
 }
