d36f6166a6cc6193153237c0dd3e5d617248cdf1
Yahia Hamitou <hamitou.yah@gmail.com>
2026-03-18 02:34:45 +0100

    Refactor index into a Model and implement TF-IDF logic

--- src/model.rs
+++ src/model.rs
@@ -1,8 +1,16 @@
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::path::{Path, PathBuf};
 
+pub type DocFreq = HashMap<String, usize>;
 pub type TermFreq = HashMap<String, usize>;
-pub type TermFreqIndex = HashMap<PathBuf, TermFreq>;
+pub type TermFreqPerDoc = HashMap<PathBuf, TermFreq>;
+
+#[derive(Default, Deserialize, Serialize)]
+pub struct Model {
+    pub tfpd: TermFreqPerDoc,
+    pub df: DocFreq,
+}
 
 pub fn tf(t: &str, d: &TermFreq) -> f32 {
     let a = d.get(t).cloned().unwrap_or(0) as f32;
@@ -10,7 +18,7 @@ pub fn tf(t: &str, d: &TermFreq) -> f32 {
     a / b
 }
 
-pub fn idf(t: &str, d: &TermFreqIndex) -> f32 {
+pub fn idf(t: &str, d: &TermFreqPerDoc) -> f32 {
     let n = d.len() as f32;
     let m = d.values().filter(|tf| tf.contains_key(t)).count().max(1) as f32;
     return (n / m).log10();
@@ -79,13 +87,13 @@ impl<'a> Iterator for Lexer<'a> {
     }
 }
 
-pub fn search_query<'a>(tf_index: &'a TermFreqIndex, query: &'a [char]) -> Vec<(&'a Path, f32)> {
+pub fn search_query<'a>(model: &'a Model, query: &'a [char]) -> Vec<(&'a Path, f32)> {
     let mut result = Vec::<(&Path, f32)>::new();
     let tokens = Lexer::new(&query).collect::<Vec<_>>();
-    for (path, tf_table) in tf_index {
+    for (path, tf_table) in &model.tfpd {
         let mut rank = 0f32;
         for token in &tokens {
-            rank += tf(&token, &tf_table) * idf(&token, &tf_index);
+            rank += tf(&token, &tf_table) * idf(&token, &model.tfpd);
         }
         result.push((path, rank));
     }
